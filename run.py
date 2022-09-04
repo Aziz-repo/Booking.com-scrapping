b2ffa83ccaed702fa9b98f5c85bc87740d18b7c3
Aziz-repo <azizhassen69@gmail.com>
2022-09-04 22:01:14 +0100

    test first sprint

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from booking.booking import Booking
 
 with Booking() as bot:
     bot.land_first_page()
-    # bot.change_currency(currency='TND')
+    bot.change_currency(currency='TND')
     bot.select_place_to_go(place_to_go="New York")
     bot.select_date(check_in_date="2022-09-04", check_out_date="2022-09-11")
     bot.select_adults(count=3)
